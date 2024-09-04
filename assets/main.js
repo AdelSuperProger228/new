20acea9ac492eeb131749fdee3d4dfcc1353b8cb
AdelSuperProger228 <sitdikov-2015@inbox.ru>
2024-09-04 17:16:15 +0300

    praktika 1

---
+++ assets/main.js
@@ -0,0 +1,11 @@
+'use strict';
+// alert("Я ДЖАВА СКРИПТ, НЕТ ТОНИ СТРАРК БЛЭТ");
+// let age = prompt('Сколько лет');
+// alert(`ВАМ ${age} лет`);
+// let isTrue
+let surname = prompt('ФАМИЛИЯ');
+let name = prompt('ИМЯ');
+let fathername = prompt('ОТЧЕСТВО');
+alert(`ВАША ФАМИЛИЯ ${surname}, ВАШЕ ИМЯ ${name},ВАШЕ ОТЧЕСТВО ${fathername}`);
+let isTrue = confirm(`Все верно?: ВАША ФАМИЛИЯ ${surname}, ВАШЕ ИМЯ ${name},ВАШЕ ОТЧЕСТВО ${fathername}`);
+alert(isTrue);
